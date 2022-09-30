607a812c2909e3767846aab9b3ccd05bd74ae856
Donatas Noreika <donoras@gmail.com>
2022-09-30 10:12:28 +0300

    pridėti admin skyriai

--- mysite/library/admin.py
+++ mysite/library/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import (Author,
+                     BookInstance,
+                     Book,
+                     Genre)
 
 # Register your models here.
+admin.site.register(Author)
+admin.site.register(Book)
+admin.site.register(BookInstance)
+admin.site.register(Genre)
--- mysite/library/models.py
+++ mysite/library/models.py
@@ -6,9 +6,15 @@ import uuid
 class Genre(models.Model):
     name = models.CharField("Pavadinimas", max_length=200, help_text="Įveskite knygos žanrą (pvz. detektyvas)")
 
+    def __str__(self):
+        return f"{self.name}"
+
 class Author(models.Model):
     first_name = models.CharField("Vardas", max_length=100)
     last_name = models.CharField("Pavardė", max_length=100)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
 
 class Book(models.Model):
     title = models.CharField("Pavadinimas", max_length=200)
@@ -16,6 +22,9 @@ class Book(models.Model):
     isbn = models.CharField("ISBN", max_length=13, help_text='13 Simbolių <a href="https://www.isbn-international.org/content/what-isbn">ISBN kodas</a>')
     author = models.ForeignKey("Author", on_delete=models.SET_NULL, null=True)
     genre = models.ManyToManyField("Genre", help_text='Išrinkite žanrą(us) šiai knygai')
+
+    def __str__(self):
+        return f"{self.author} - {self.title}"
 
 class BookInstance(models.Model):
     uuid = models.UUIDField(default=uuid.uuid4, help_text='Unikalus UUID knygos kopijai')
@@ -25,3 +34,7 @@ class BookInstance(models.Model):
     LOAN_STATUS = (('a', 'Administruojama'), ('p', 'Paimta'), ('g', 'Galima paimti'), ('r', 'Rezervuota'))
 
     status = models.CharField(max_length=1, choices=LOAN_STATUS, blank=True, default='a', help_text="Statusas")
+
+
+    def __str__(self):
+        return f"{self.book} ({self.uuid}) - {self.status} ({self.due_back})"
