60b4c78aeb9b73107f6ded5f4f033720f2e9e169
Donatas Noreika <donoras@gmail.com>
2022-10-03 09:25:15 +0300

    2 paskaita: pridėti verbose prie modelių

--- mysite/library/models.py
+++ mysite/library/models.py
@@ -9,12 +9,22 @@ class Genre(models.Model):
     def __str__(self):
         return f"{self.name}"
 
+    class Meta:
+        verbose_name = 'Žanras'
+        verbose_name_plural = 'Žanrai'
+
+
 class Author(models.Model):
     first_name = models.CharField("Vardas", max_length=100)
     last_name = models.CharField("Pavardė", max_length=100)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
+
+    class Meta:
+        verbose_name = 'Autorius'
+        verbose_name_plural = 'Autoriai'
+
 
 class Book(models.Model):
     title = models.CharField("Pavadinimas", max_length=200)
@@ -25,6 +35,10 @@ class Book(models.Model):
 
     def __str__(self):
         return f"{self.author} - {self.title}"
+
+    class Meta:
+        verbose_name = 'Knyga'
+        verbose_name_plural = 'Knygos'
 
 class BookInstance(models.Model):
     uuid = models.UUIDField(default=uuid.uuid4, help_text='Unikalus UUID knygos kopijai')
@@ -38,3 +52,7 @@ class BookInstance(models.Model):
 
     def __str__(self):
         return f"{self.book} ({self.uuid}) - {self.status} ({self.due_back})"
+
+    class Meta:
+        verbose_name = 'Knygos egzempliorius'
+        verbose_name_plural = 'Knygų egzemplioriai'
